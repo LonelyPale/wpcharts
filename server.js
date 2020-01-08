16f44d9699b580e692573be6c32a37ec48cca5cf
LonelyPale <lonelypale@126.com>
2020-01-08 12:07:50 +0800

    update server.js -> watch

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ let params = {
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
     root: ".", // Set root directory that's being served. Defaults to cwd.
     open: false, // When false, it won't load your browser by default.
-    ignore: './.idea', // comma-separated string for paths to ignore
+    watch: ['data', 'dist', 'lib', './*.html'],
+    ignore: '.idea', // comma-separated string for paths to ignore
     file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
     mount: [['/wpcharts', '.']], // Mount a directory to a route.
